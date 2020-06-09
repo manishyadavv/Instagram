575e2783a294d5badeeea50bb443b0b11cda33d8
manishyadavv <manishyadav.nd@gmail.com>
2020-06-10 01:30:22 +0530

    middleware using app.use()and passing in get

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ const app=express();
 
 PORT=5000;
 
-app.get('/home',(req,res)=>{
+//middleware
 
+const customMiddleware =(req,res,next)=>{
+    console.log("middleware executed!!")
+    next()
+
+}
+
+
+
+//app.use(customMiddleware)
+
+
+app.get('/',(req,res)=>{
+    console.log("home")
     res.send("hello world")
 });
 
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+    res.send("about page")
+});
 
 
 app.listen(PORT,()=>{
