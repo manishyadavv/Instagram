a5198e81aea217f5d8166163fe3b60e5740c061c
manishyadavv <manishyadav.nd@gmail.com>
2020-06-10 02:28:22 +0530

    mongo atlas connection added and mongoose package used

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 
-const express =require("express");
+const express = require("express");
 const app=express();
+const mongoose = require("mongoose");
+const PORT=5000;
+const {MONGOURI} = require('./keys')
 
-PORT=5000;
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+        useUnifiedTopology: true
+})
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo")
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("error connecting",err)
+})
 
 //middleware
 
