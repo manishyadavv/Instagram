91d87e789d22a519e5731cdae7f0c876ae01a258
manishyadavv <manishyadav.nd@gmail.com>
2020-06-10 02:36:09 +0530

    user schema defined

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const PORT=5000;
 const {MONGOURI} = require('./keys')
 
 
+require('./models/user')
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
         useUnifiedTopology: true
 })
+
 mongoose.connection.on('connected',()=>{
     console.log("connected to mongo")
 })
+
 
 mongoose.connection.on('error',(err)=>{
     console.log("error connecting",err)
