d3b14d6842a3dc4d5c4239d1952d9b5a65e5ea79
manishyadavv <manishyadav.nd@gmail.com>
2020-06-10 12:41:33 +0530

    signin post method created

--- routes/auth.js
+++ routes/auth.js
@@ -54,4 +54,44 @@ router.post('/signup',(req,res)=>{
 })
 
 
+
+
+router.post('/signin',(req,res)=>{
+
+    const {email,password}=req.body
+
+    if(!email||!password){
+        return req.status(422).json({error:"please add email and password"})
+    }
+
+    User.findOne({email:email})
+        .then(savedUser=>{
+            if(!savedUser)
+            {
+                return res.status(422).json({error:"Invalid Email or Password"})
+            }
+
+            bcrypt.compare(password,savedUser.password)
+                .then(doMatch=>{
+                    if(doMatch)
+                    {
+                        res.json({message:"successfully signed in"})
+                    }
+                    else{
+                        return res.status(422).json({error:"Invalid Email or Password"})
+                    }
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+        })
+
+
+
+
+})
+
+
+
+
 module.exports=router
