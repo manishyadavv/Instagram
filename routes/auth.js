744fac7a89c0d49bf40e88a874b7bc6e43aac1cd
manishyadavv <manishyadav.nd@gmail.com>
2020-06-10 12:05:52 +0530

    posting data on Mongo atlas

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express= require("express")
 const router = express.Router()
 
+const mongoose= require ('mongoose')
+
+const User = mongoose.model('User')
 
 router.get("/",(req,res)=>{
     res.send("hello")
@@ -14,9 +17,34 @@ router.post('/signup',(req,res)=>{
     if( !email || !password|| !name){
        return res.status(422).json({error:"please add all fields"})
     }
-    res.json({message:"successfully posted"})
 
+    //if a user with same email exits
+    //querying in Db
+    User.findOne({email:email})
+        .then((savedUser)=>{
+            if(savedUser){
+                return res.status(422).json({error:"user already exists"})
+            }
+
+            const user= new User({
+                email:email,
+                password:password,
+                name:name
+            })
+            user.save()
+                .then(user=>{
+                    res.json({message:"saved successfulyy"})
+                })
+                .catch(err=>{
+                    console.log(err)
+
+                })
+        })
+        .catch(err=>{
+            console.log(err)
+        })
 
 })
+
 
 module.exports=router
